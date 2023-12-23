3e51b1399af3c462826fbdc2ee83e8a90d241489
Allan Tokuda <allan.tokuda@gmail.com>
2023-12-22 23:49:47 -0600

    Piano-like graph for kicks

--- src/lib/components/listen.svelte
+++ src/lib/components/listen.svelte
@@ -6,20 +6,27 @@
   let freqs = []
 
   const SCALE = 1.345825195
+  const C2 = 65.4
+
   function indexOfFreq(freq) {
     return Math.round(freq/SCALE)
   }
 
-  function colorForIndex(index) {
-    return [1,4,6,9,11].includes(index % 12) ? 'black' : 'white';
+  function noteForIndex(index) {
+    return ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'][index % 12]
   }
+
+  function colorForIndex(index) {
+    return noteForIndex(index).includes('#') ? 'black' : 'white';
+  }
+
 
   function startAudio() {
     if (browser) {
       window.Tone = Tone
 
       const nums = new Array(60).fill().map((_, i) => i);
-      let freqs = nums.map((i) => 55 * 2**(i/12))
+      let freqs = nums.map((i) => C2 * 2**(i/12))
       let indices = freqs.map((f) => indexOfFreq(f))
       let fft = new Tone.FFT(16384)
       let mic = new Tone.UserMedia()
@@ -38,8 +45,36 @@
 
 <button on:click={startAudio}>Start</button>
 
-<div>
+<div class="graph">
 {#each values as value, i}
-  <div style="height: {100+value}px; width: 10px; background-color: {colorForIndex(i)}; border: 1px solid gray; display: inline-block;"></div>
+  <div class="bar {colorForIndex(i)}" style="height: {120+value}px;">
+    {noteForIndex(i)}
+  </div>
 {/each}
 </div>
+
+<style>
+  .graph {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-end;
+    height: 200px;
+  }
+  .bar {
+    width: 24px;
+    text-align: center;
+    border: 1px solid gray;
+    display: inline-block;
+    min-height: 35px;
+    overflow-wrap: anywhere;
+  }
+  .bar.black {
+    background-color: black;
+    color: white;
+  }
+  .bar.white {
+    background-color: white;
+    color: black;
+  }
+</style>
