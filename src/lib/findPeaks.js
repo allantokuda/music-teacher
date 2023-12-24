bfba47aab2c52df2af9264e4cea2f3405a9e92c6
Allan Tokuda <allan.tokuda@gmail.com>
2023-12-24 07:56:25 -0600

    Local neighborhood max

--- src/lib/findPeaks.js
+++ src/lib/findPeaks.js
@@ -1,35 +1,25 @@
 // Find peaks in FFT data
 export default function findPeaks(fftGains) {
+  // First find values larger value than their immediate neighbors.
+  // This is not robust to noise.
+  // It returns 50 to 100 peaks within this 5 octave range.
   let peaks = []
-
-  let upSteps = 0;
-  let downSteps = 0;
-  let peakCandidate = null;
   for (let i = 49; i<1468; i++) {
-    const n = Math.floor(i/35.66)
-    // Define a peak as the middle of a sequence of numbers
-    // where there are n upward steps and then n downward steps,
-    // where n is a fraction of the index i that is approximately one musical quarter step
-    // (for an FFT of size 16384 from a 22050 Hz sample rate)
-    // Loop through a usable range of indices,
-    // remember how many upward steps have been taken in a row
-    // and then how many downward steps have been taken in a row.
-    // When that sequence is reached, add the peak index to the peaks list.
-    // Any upward step resets the downward step count to 0; same vice-versa.
-    if (fftGains[i] > fftGains[i-1]) {
-      upSteps++;
-      downSteps = 0;
-    } else if (fftGains[i] < fftGains[i-1]) {
-      if (upSteps >= n) {
-        peakCandidate = i - 1;
-      }
-      downSteps++;
-      upSteps = 0;
-      if (downSteps === n && peakCandidate) {
-        peaks.push(peakCandidate);
-        peakCandidate = null;
-      }
+    if (fftGains[i] > fftGains[i-1] && fftGains[i] > fftGains[i+1]) {
+      peaks.push(i);
     }
   }
-  return peaks;
+
+  // This filters to only the peaks that are the highest in their local neighborhood.
+  // This returns about 20 to 40 peaks.
+  let goodPeaks = [];
+  for (let p = 0; p<peaks.length; p++) {
+    let i = peaks[p];
+    const n = Math.floor(i/35.66)
+    if (fftGains[i] === Math.max(...fftGains.slice(i-n, i+n))) {
+      goodPeaks.push(i);
+    }
+  }
+  //console.log(goodPeaks.length);
+  return goodPeaks;
 }
