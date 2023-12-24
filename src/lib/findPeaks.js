5321bded3cd3d845f94b70e6950d657e6b0b83c9
Allan Tokuda <allan.tokuda@gmail.com>
2023-12-24 08:11:47 -0600

    Refactor to one loop

--- src/lib/findPeaks.js
+++ src/lib/findPeaks.js
@@ -8,25 +8,19 @@ export default function findPeaks(fftGains) {
   let peaks = []
   for (let i = 49; i<1468; i++) {
     if (fftGains[i] > fftGains[i-1] && fftGains[i] > fftGains[i+1]) {
-      peaks.push(i);
+      const n = Math.floor(i/35.66)
+      const localArea = fftGains.slice(i-n, i+n);
+      const localMax = Math.max(...localArea);
+      const localMin = Math.min(...localArea);
+      if (fftGains[i] === localMax &&
+        fftGains[i] > fftGains[i-n] + THRESHOLD &&
+        fftGains[i] > fftGains[i+n] + THRESHOLD) {
+        peaks.push(i);
+        //peaks.push({ i, height: localMax - localMin });
+      }
     }
   }
-
-  // Filter to only the peaks that are the highest in their local neighborhood. This returns about 20 to 40 peaks.
-  // Filter further to only the peaks that are higher than their neighbors by a certain threshold. This returns about 3 to 15 peaks.
-  let goodPeaks = [];
-  for (let p = 0; p<peaks.length; p++) {
-    let i = peaks[p];
-    const n = Math.floor(i/35.66)
-    const localArea = fftGains.slice(i-n, i+n);
-    const localMax = Math.max(...localArea);
-    const localMin = Math.min(...localArea);
-    if (fftGains[i] === localMax &&
-      fftGains[i] > fftGains[i-n] + THRESHOLD &&
-      fftGains[i] > fftGains[i+n] + THRESHOLD) {
-      goodPeaks.push(i);
-    }
-  }
-  //console.log(goodPeaks.length);
-  return goodPeaks;
+  //return peaks.sort((a, b) => b.height - a.height).map((p) => p.i); // in case I need the largest peaks
+  //console.log(peaks.length);
+  return peaks;
 }
