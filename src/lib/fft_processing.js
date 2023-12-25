00055970d62d1e3bf6d9a5bd261dbfd4cad720f0
Allan Tokuda <allan.tokuda@gmail.com>
2023-12-24 18:30:00 -0600

    Freq of index

--- src/lib/fft_processing.js
+++ src/lib/fft_processing.js
@@ -1,11 +1,15 @@
 const C2_Freq = 65.4;
+const SCALE = 1.345825195; // 22050 sample rate and 16384 fft size
 const integer_series = new Array(60).fill().map((_, i) => i);
 export const noteFreqs = integer_series.map((i) => C2_Freq * 2**(i/12))
 export const noteIndices = noteFreqs.map((f) => indexOfFreq(f))
 
 export function indexOfFreq(freq) {
-  // for 22050 sample rate and 16384 fft size
-  return Math.round(freq/1.345825195)
+  return Math.round(freq / SCALE);
+}
+
+export function freqOfIndex(fftIndex) {
+  return fftIndex * SCALE;
 }
 
 export function noteGains(fftGains) {
