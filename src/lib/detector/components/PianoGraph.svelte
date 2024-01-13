3c864eea1bed548a6f2368d448eb52a640a872d4
Allan Tokuda <allan.tokuda@gmail.com>
2024-01-13 17:00:14 -0600

    Much better ln curve fit

--- src/lib/detector/components/PianoGraph.svelte
+++ src/lib/detector/components/PianoGraph.svelte
@@ -13,7 +13,7 @@
 
 <div class="piano-graph">
   {#each pitch_gains as gain, n}
-    <div class="bar {pitchColor(n)} {pitchHighlight(n, highlight_pitch_numbers)}" style="height: {240+gain}px;">
+    <div class="bar {pitchColor(n)} {pitchHighlight(n, highlight_pitch_numbers)}" style="height: {80+gain}px;">
       {pitchName(n)}
     </div>
   {/each}
--- src/lib/detector/functions/step1_stackOvertones.ts
+++ src/lib/detector/functions/step1_stackOvertones.ts
@@ -8,10 +8,24 @@ const result_indices = integerArray(FFT_SIZE/NUM_OVERTONES);
 const multiples = integerArray(NUM_OVERTONES).map((i) => i+1);
 
 function normalize(fftGains: number[]): number[] {
-  // Work in the higher frequency half of the data which will mostly have uniform noise and avoids low-frequency artifacts
-  const avgGain = fftGains.slice(1000, 2000).reduce((sum, gain) => sum + gain, 0) / 1000;
-  const scale = avgGain / (1 + Math.log(1500)) * 0.5;
-  return fftGains.map((gain, i) => gain - (scale * (1 + Math.log(i+1))));
+  const { A, B } = lnCurveFit(fftGains);
+  const normalized = fftGains.map((gain, i) => gain - (A + B * Math.log(i+1)));
+  return normalized;
+}
+
+// Accepts a list of values (y) and returns
+// constants A and B for the formula: y = A + B * ln(i)
+// representing a natural logarithmic curve fit (y) to the input values.
+// The matrix math doesn't care that it's ln(i) so for this, x = ln(i)
+function lnCurveFit(values: number[]): { A: number, B: number } {
+  const sumX = values.reduce((sum, _, i) => sum + Math.log(i+1), 0);
+  const sumY = values.reduce((sum, value) => sum + value, 0);
+  const sumXY = values.reduce((sum, value, i) => sum + value * Math.log(i+1), 0);
+  const sumX2 = values.reduce((sum, value, i) => sum + Math.log(i+1) ** 2, 0);
+  const N = values.length;
+  const B = (N * sumXY - sumX * sumY) / (N * sumX2 - sumX ** 2);
+  const A = (sumY - B * sumX) / N;
+  return { A, B };
 }
 
 export default function step1_stackOvertones(fftGains: Float32Array): number[] {
