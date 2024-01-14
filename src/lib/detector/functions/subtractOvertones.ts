e3f08d861755ef5e056fa550645718a527893ed9
Allan Tokuda <allan.tokuda@gmail.com>
2024-01-14 10:03:14 -0600

    Clearer use of void to mutate array instead of copying

--- src/lib/detector/functions/subtractOvertones.ts
+++ src/lib/detector/functions/subtractOvertones.ts
@@ -6,25 +6,22 @@ function gaussian(x: number, mean: number, std: number): number {
   return Math.exp(-((x - mean) ** 2) / (2 * std ** 2));
 }
 
-function subtractGaussianPeak(fft_gains: number[], freq: number): number[] {
+// Pass gains array by reference and modify it in place
+function subtractGaussianPeak(fft_gains: number[], freq: number): void {
   const center_index = Math.floor(freq / SCALE);
   const min_index = Math.floor(freq * 0.97 / SCALE);
   const max_index = Math.floor(freq * 1.03 / SCALE);
-  const gains_sample = fft_gains.slice(min_index, max_index - min_index);
-  const max_gain = Math.max(...gains_sample);
+  const gains_sample = fft_gains.slice(min_index, max_index);
+  const max_gain = gains_sample.reduce((max, n) => n > max ? n : max, 0);
   const std = PEAK_STANDARD_DEVIATION_SCALE * center_index;
-  let result = fft_gains;
   for (let i = min_index; i <= max_index; i++) {
-    result[i] -= gaussian(i, center_index, std) * max_gain;
+    fft_gains[i] -= gaussian(i, center_index, std) * max_gain;
   }
-  return result;
 }
 
-export default function subtractOvertones(fft_gains: number[], tonic_freq: number): number[] {
-  let result = fft_gains;
-  [0.25, 0.5, 1, 2, 3, 4, 5, 6, 7, 8].forEach((multiple) => {
+export default function subtractOvertones(fft_gains: number[], tonic_freq: number): void {
+  [0.25, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].forEach((multiple) => {
     const overtone_freq = tonic_freq * multiple;
-    subtractGaussianPeak(result, overtone_freq);
+    subtractGaussianPeak(fft_gains, overtone_freq);
   })
-  return result;
 }
